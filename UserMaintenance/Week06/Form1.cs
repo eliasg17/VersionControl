40b170d08f85ac1d899e52a7994f3576571dbf8f
elias <elias@DESKTOP-V3MNLS0>
2020-10-13 15:30:36 +0200

    4

--- UserMaintenance/Week06/Form1.cs
+++ UserMaintenance/Week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Week06.Entities;
 using Week06.MnbServiceReference;
 
@@ -34,7 +35,24 @@ namespace Week06
 
             var result = response.GetExchangeRatesResult;
 
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
 
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
