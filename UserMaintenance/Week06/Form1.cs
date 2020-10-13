454064c656f468ac4054acc590579ef96cd2700f
elias <elias@DESKTOP-V3MNLS0>
2020-10-13 15:37:49 +0200

    5

--- UserMaintenance/Week06/Form1.cs
+++ UserMaintenance/Week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Week06.Entities;
 using Week06.MnbServiceReference;
@@ -53,6 +54,22 @@ namespace Week06
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
