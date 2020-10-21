0c4d30e8ff260352939a2fc8da6262ad9ba14655
elias <elias@DESKTOP-V3MNLS0>
2020-10-21 20:38:08 +0200

    5

--- UserMaintenance/Mikroszim/Form1.cs
+++ UserMaintenance/Mikroszim/Form1.cs
@@ -51,9 +51,9 @@ namespace Mikroszim
                     birthProbability.Add(new BirthProbability()
                     {
                         Age = int.Parse(line[0]),
-                        NbrOfChildren = int.Parse(line[2]),
-                        //P =
-                    }) ;
+                        NbrOfChildren = int.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
                 }
             }
 
@@ -73,13 +73,15 @@ namespace Mikroszim
                     {
                         Age = int.Parse(line[0]),
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
-                        //P =
+                        P = double.Parse(line[2])
                     });
                 }
             }
 
             return deathProbability;
         }
+
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
@@ -87,7 +89,45 @@ namespace Mikroszim
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
-        }
 
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep();
+                }
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
     }
 }
