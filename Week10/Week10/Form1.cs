8618112b7274fed467dd403cb66895b6ac5e0f78
elias <elias@DESKTOP-V3MNLS0>
2020-11-24 19:33:43 +0100

    2

--- Week10/Week10/Form1.cs
+++ Week10/Week10/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Week10
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
+
+            int populationSize = 100;
+            int nbrOfSteps = 10;
+            int nbrOfStepsIncrement = 10;
+            int generation = 1;
         }
     }
 }
